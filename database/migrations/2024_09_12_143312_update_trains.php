119f9cad0f5568babeee544ced16cccd1f2f798f
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-12 16:50:11 +0200

    Build: update database

---
+++ database/migrations/2024_09_12_143312_update_trains.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->time('departure_time', precision: 0)->after('departure_date');
+            $table->time('arrive_time', precision: 0)->after('arrive_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('departure_time');
+            $table->dropColumn('arrive_time');
+        });
+    }
+};
