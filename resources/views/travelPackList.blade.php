7b5f64291e28496b3a83e22571efe65ca4121e75
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-13 22:03:54 +0200

    feat:print travel_pack table

--- resources/views/travelPackList.blade.php
+++ resources/views/travelPackList.blade.php
@@ -7,9 +7,29 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Destination</th>
+                    <th scope="col">Description</th>
+                    <th scope="col" class="text-nowrap">Departure date</th>
+                    <th scope="col" class="text-nowrap">Arrive date</th>
+                    <th scope="col">Price</th>
                 </tr>
             </thead>
+            @foreach ($travel_packs as $pack)
+                <tr>
+                    @php
+                        $departure_date = date_create($pack->departure_date);
+                        $arrive_date = date_create($pack->arrive_date);
+                    @endphp
+                    <td>{{ $pack->name }}</td>
+                    <td>{{ $pack->destination }}</td>
+                    <td>{{ $pack->description }}</td>
+                    <td>{{ $departure_date->format('Y-m-d') }}</td>
+                    <td>{{ $arrive_date->format('Y-m-d') }}</td>
+                    <td class="d-flex flex-column align-items-center">{{ $pack->price }}&euro; <a href="#"
+                            class="btn btn-danger">Prenota</a></td>
+                </tr>
+            @endforeach
             <tbody>
             </tbody>
         </table>
